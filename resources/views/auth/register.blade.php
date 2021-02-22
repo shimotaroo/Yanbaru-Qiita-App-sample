ea6d1b4bf95196a093c02bd539ce0081b06490e4
shimotaroo <shimotaro.0327@gmail.com>
2021-02-22 23:02:52 +0900

    見出しをh2→h3に変更、その他誤記修正等

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">
+                    <h3 class="text-center my-2">{{ __('Register User') }}</h3>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -30,10 +32,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Term') }}<span class="text-danger">(※)</span></label>
+                            <label for="term" class="col-md-4 col-form-label text-md-right">{{ __('Term') }}<span class="text-danger">(※)</span></label>
 
                             <div class="col-md-6">
-                                <input id="email" type="number" class="form-control @error('term') is-invalid @enderror" name="term" value="{{ old('term') }}" required autocomplete="term" placeholder="3" min="1">
+                                <input id="term" type="number" class="form-control @error('term') is-invalid @enderror" name="term" value="{{ old('term') }}" required autocomplete="term" placeholder="3" min="1">
                                 <small>半角数字2桁以内で入力してください。</small>
                                 @error('term')
                                     <span class="invalid-feedback" role="alert">
@@ -85,7 +87,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-5 offset-md-5">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('登録') }}
+                                    {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
