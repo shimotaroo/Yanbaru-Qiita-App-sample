ab133ca82f6a2a9318cfc57ce2b7847ab4ae5454
shimotaroo <shimotaro.0327@gmail.com>
2021-02-22 23:03:09 +0900

    見出しをh2→h3に変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">
+                    <h3 class="text-center my-2">{{ __('Login') }}</h3>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
