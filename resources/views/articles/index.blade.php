d590a8786eafb827c62056a62e72087c191b6ba6
shimotaroo <shimotaro.0327@gmail.com>
2021-02-28 13:14:53 +0900

    一覧画面のレイアウト変更（2カラム化、表示項目の場所）

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             
             {{-- フラッシュメッセージ --}}
             @if (session('flashMsg'))
@@ -17,56 +17,60 @@
 
             {{-- 記事が登録されている場合 --}}
             @if (!$articles->isEmpty())
+            <div class="row">
                 @foreach ($articles as $article)
-                    <div class="card mb-5">
-                        <div class="card-header d-flex justify-content-between align-items-center">
-                            <div>
-                                <i class="fas fa-user-edit mr-3"></i>{{ __('DateTime') }}：{{ $article->created_at->format('Y-m-d') }}
-                            </div>
-                            <div class="d-flex justify-content-around">
-                                @if ($article->user_id === Auth::id())
-                                    <a  href="{{ route('articles.edit', $article) }}" class="btn btn-secondary rounded-pill ml-auto mr-2">編集</a>
-                                    <form action="{{ route('articles.destroy', $article) }}" method="POST" id="delete-form">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="button" class="btn btn-danger rounded-pill ml-auto" id="delete-btn">削除</button>                                
-                                    </form>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <p class="col-md-4 text-md-right">{{ __('Name') }}</p>
-                                <p class="col-md-6">
-                                    {{ $article->user->name }}
-                                </p>
-                            </div>
-                            <div class="row">
-                                <p class="col-md-4 text-md-right">{{ __('Term') }}</p>
-                                <p class="col-md-6">
-                                    {{ $article->user->term }}期生
-                                </p>
-                            </div>
-                            <div class="row">
-                                <p class="col-md-4 text-md-right">{{ __('Title') }}</p>
-                                <p class="col-md-6">
-                                    {{ $article->title }}
-                                </p>
-                            </div>
-                            <div class="row">
-                                <p class="col-md-4 text-md-right">{{ __('URL') }}</p>
-                                <p class="col-md-6">
-                                    <a href="{{ $article->url }}" target="_blank">{{ $article->url }}</a>
-                                </p>
-                            </div>
-                            <form method="GET" action="{{ route('index') }}">
-                                <div class="row">
-                                    <a href="{{ route('articles.show', $article) }}" class="btn btn-success text-white col-md-4 mx-auto">詳細を見る</a>
+                    <div class="col-md-6">
+                        <div class="card mb-5">
+                            <div class="card-header d-flex justify-content-between align-items-center">
+                                <div class="font-weight-bold">
+                                    <i class="fas fa-user-edit mr-2"></i>{{ $article->user->name }}
                                 </div>
-                            </form>
+                                <div class="d-flex justify-content-around">
+                                    @if ($article->user_id === Auth::id())
+                                        <a  href="{{ route('articles.edit', $article) }}" class="btn btn-secondary rounded-pill ml-auto mr-2">編集</a>
+                                        <form action="{{ route('articles.destroy', $article) }}" method="POST" id="delete-form">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="button" class="btn btn-danger rounded-pill ml-auto" id="delete-btn">削除</button>                                
+                                        </form>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <p class="col-md-4 text-md-right">{{ __('Term') }}</p>
+                                    <p class="col-md-6">
+                                        {{ $article->user->term }}期生
+                                    </p>
+                                </div>
+                                <div class="row">
+                                    <p class="col-md-4 text-md-right">{{ __('Title') }}</p>
+                                    <p class="col-md-6">
+                                        {{ $article->title }}
+                                    </p>
+                                </div>
+                                <div class="row">
+                                    <p class="col-md-4 text-md-right">{{ __('URL') }}</p>
+                                    <p class="col-md-6">
+                                        <a href="{{ $article->url }}" target="_blank">{{ $article->url }}</a>
+                                    </p>
+                                </div>
+                                <div class="row">
+                                    <p class="col-md-4 text-md-right">{{ __('DateTime') }}</p>
+                                    <p class="col-md-6">
+                                        {{ $article->created_at->format('Y-m-d') }}
+                                    </p>
+                                </div>
+                                <form method="GET" action="{{ route('index') }}">
+                                    <div class="row">
+                                        <a href="{{ route('articles.show', $article) }}" class="btn btn-success text-white col-md-4 mx-auto">詳細を見る</a>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                @endforeach
+                    @endforeach
+                </div>
             {{-- 記事が登録されていない場合 --}}
             @else
                 <div class="text-center mt-4">
