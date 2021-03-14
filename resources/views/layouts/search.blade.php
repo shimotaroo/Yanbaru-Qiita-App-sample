478c037a0028725d192681a5cae61723d157d0d5
yoshitaro-yoyo <tsute3842@gmail.com>
2021-03-15 01:00:11 +0900

    検索機能追加 #21：検索結果の表示時 セレクトボックス・入力フォーム検索条件保持

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -1,12 +1,14 @@
 {{-- 検索 --}}
-<form action="" method="GET">
+<form action="{{ route('searchArticles') }}" method="GET">
+    {{ csrf_field() }}
     <div class="col-md-6 mx-auto mb-5">
         <div class="row">
             <p class="col-md-4 text-md-right">{{ __('Term') }}</p>
             <div class="col-md-6">
-                <select name="term" id="" class="mr-2">
-                    @foreach (config('const') as $termNumber)
-                        <option value="{{ $termNumber }}">{{ $termNumber }}</option>
+                <select name="term" id="term" class="mr-2">
+                    <option></option>
+                    @foreach (config('const') as $index => $termNumber)
+                        <option value="{{ $index }}" @if(old('term', $selectTerm ?? '') == $index) selected @endif>{{ $termNumber }}</option>
                     @endforeach
                 </select>
                 期生
@@ -15,21 +17,18 @@
         <div class="row">
             <p class="col-md-4 text-md-right">{{ __('Category') }}</p>
             <div class="col-md-6">
-                {{-- TODO：仮で作っているので適宜変更してください --}}
-                <select name="" id="">
-                    <option value=""></option>
-                    <option value="1">laravel</option>
-                    <option value="2">PHP</option>
-                    <option value="3">Docker</option>
-                    <option value="4">web基礎</option>
+                <select name="category">
+                    <option></option>
+                    @foreach ($categoryForSelects as $select)
+                        <option value="{{ $select }}" @if(old('category', $selectCategory ?? '') == $select) selected @endif>{{ $select }}</option>
+                    @endforeach
                 </select>
             </div>
         </div>
         <div class="row">
             <p class="col-md-4 text-md-right">{{ __('Free Word') }}</p>
             <div class="col-md-6">
-                {{-- TODO：仮で作っているので適宜変更してください --}}
-                <input type="text">
+                <input type="text" name="word" value="{{ $selectWord ?? '' }}">
             </div>
         </div>
         <button type="submit" class="btn btn-block btn-success col-md-4 mx-auto py-2 mt-3">
