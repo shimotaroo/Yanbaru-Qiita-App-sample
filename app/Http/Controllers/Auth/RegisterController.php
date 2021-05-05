7bd5e666e2ef4d3d021aa6e74c8af165dc4e75d6
yoshitaro-yoyo <tsute3842@gmail.com>
2021-05-05 16:28:40 +0900

    modi:正規表現の変更と追加、デミリタ・正規表現修飾子

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'term' => ['required', 'digits_between:1,2'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'regex:^([a-zA-Z0-9]{8,})$^', 'confirmed'],
+            'password' => ['required', 'string', 'regex:/^([a-zA-Z0-9]{8,})+$/u', 'confirmed'],
         ]);
     }
 
